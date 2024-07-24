2277b910901954da9b84ad046c4b0946650c9200
safiraferia <safiraamalina85@gmail.com>
2024-07-24 21:47:44 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -38,11 +38,11 @@ with col2:
 # Kode untuk prediksi
 diab_diagnosis = ''
 
-# Membuat tombol untuk prediksi
+# membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-    if diab_prediction[0] == 1):
+    if(diab_prediction[0] == 1):
         diab_diagnosis = 'Pasien terkena Diabetes'
     else:
         diab_diagnosis = 'Pasien tidak terkena Diabetes'
