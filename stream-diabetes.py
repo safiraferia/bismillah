f2b1e5fd7b92dd6b6cf2c9d732069610e5ffd383
safiraferia <safiraamalina85@gmail.com>
2024-07-24 22:01:22 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -5,35 +5,34 @@ import streamlit as st
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
 # Judul web
-st.title('Data Mining Prediksi Diabetes')
-st.write('Aplikasi ini membantu dalam memprediksi kemungkinan diabetes berdasarkan parameter medis yang Anda masukkan.')
+st.title('Prediksi Diabetes Sekarang, Yuk!')
 
 # Membagi kolom
 col1, col2 = st.columns(2)
 
 with col1:
-    Pregnancies = st.text_input('Input nilai Pregnancies')
+    Pregnancies = st.text_input('Input Angka Kehamilan')
 
 with col2:
-    Glucose = st.text_input('Input nilai Glucose')
+    Glucose = st.text_input('Input Nilai Glukosa')
 
 with col1:
-    BloodPressure = st.text_input('Input nilai Blood Pressure')
+    BloodPressure = st.text_input('Input Angka Tekanan Darah')
 
 with col2:
-    SkinThickness = st.text_input('Input nilai Skin Thickness')
+    SkinThickness = st.text_input('Input Angka Ketebalan Lipatan Kulit')
 
 with col1:
-    Insulin = st.text_input('Input nilai Insulin')
+    Insulin = st.text_input('Input Angka Insulin')
 
 with col2:
-    BMI = st.text_input('Input nilai BMI')
+    BMI = st.text_input('Input Angka BMI')
 
 with col1:
-    DiabetesPedigreeFunction = st.text_input('Input nilai Diabetes Pedigree Function')
+    DiabetesPedigreeFunction = st.text_input('Input Indikator Riwayat Diabetes')
 
 with col2:
-    Age = st.text_input('Input nilai Age')
+    Age = st.text_input('Input Umur')
 
 # Kode untuk prediksi
 diab_diagnosis = ''
@@ -43,9 +42,9 @@ if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
     if(diab_prediction[0] == 1):
-        diab_diagnosis = '**Pasien terkena Diabetes**'
+        diab_diagnosis = 'Pasien terkena Diabetes'
     else:
-        diab_diagnosis = '**Pasien tidak terkena Diabetes**'
+        diab_diagnosis = 'Pasien tidak terkena Diabetes'
 
 # Menampilkan hasil di tengah
 st.markdown(f"""
